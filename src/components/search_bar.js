ba0b89be116dd33ed598078301c26b2d4cee5214
Jason Holmberg <jason.holmberg@experticity.com>
2017-01-07 09:56:59 -0700

    controlled component

--- src/components/search_bar.js
+++ src/components/search_bar.js
@@ -4,18 +4,25 @@ import React, { Component } from 'react';
  * The search bar.
  */
 class SearchBar extends Component {
-  render() {
-    return <input onChange={this.onInputChange} />;
+
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      term: ''
+    };
   }
 
-  /**
-   * Handle changes to the input element.
-   *
-   * @param event - the  genertic synthetic event object
-   */
-  onInputChange(event) {
-    console.log(event.target.value);
+  render() {
+    return (
+      <div>
+        <input
+          value={this.state.term}
+          onChange={event => this.setState({ term: event.target.value })} />
+      </div>
+      );
   }
+
 }
 
 export default SearchBar;
