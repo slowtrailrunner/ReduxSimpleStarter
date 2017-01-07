ee16a55bfe48898be820368a7428ab896b5fdc05
Jason Holmberg <jason.holmberg@experticity.com>
2017-01-07 09:24:44 -0700

    up to section 15

---
+++ src/components/search_bar.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+
+/**
+ * The search bar.
+ */
+class SearchBar extends Component {
+  render() {
+    return <input />;
+  }
+}
+
+export default SearchBar;
--- src/index.js
+++ src/index.js
@@ -1,15 +1,18 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { Provider } from 'react-redux';
-import { createStore, applyMiddleware } from 'redux';
 
-import App from './components/app';
-import reducers from './reducers';
+import SearchBar from './components/search_bar';
 
-const createStoreWithMiddleware = applyMiddleware()(createStore);
+const API_KEY = 'AIzaSyD3cSXAAH1GKSoUS7twGHh59cHxcVU5YU0';
 
-ReactDOM.render(
-  <Provider store={createStoreWithMiddleware(reducers)}>
-    <App />
-  </Provider>
-  , document.querySelector('.container'));
+// Create a new componet.  This should produce some HTML
+const App = () => {
+  return (
+    <div>
+      <SearchBar />
+    </div>
+  );
+}
+
+// Take this componet's generated HTML and put it on the page (in the DOM)
+ReactDOM.render(<App />, document.querySelector('.container'));
