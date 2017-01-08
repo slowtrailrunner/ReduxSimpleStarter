89aba019270dad100ebf636d2285b7dba00ce855
Jason Holmberg <jason.holmberg@experticity.com>
2017-01-08 08:34:00 -0700

    Embed and select a video

--- src/index.js
+++ src/index.js
@@ -2,6 +2,8 @@ import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
 import YTSearch from 'youtube-api-search';
 import SearchBar from './components/search_bar';
+import VideoList from './components/video_list';
+import VideoDetail from './components/video_detail';
 
 const API_KEY = 'AIzaSyD3cSXAAH1GKSoUS7twGHh59cHxcVU5YU0';
 
@@ -11,11 +13,15 @@ class App extends Component {
 
     this.state = {
       videos: [],
+      selectedVideo: null,
     }
 
     // Run and initial search
     YTSearch({ key: API_KEY, term: 'surfboards' }, (videos) => {
-      this.setState({ videos });
+      this.setState({
+        videos: videos,
+        selectedVideo: videos[0],
+       });
     });
   }
 
@@ -23,6 +29,11 @@ class App extends Component {
     return (
       <div>
         <SearchBar />
+        <VideoDetail video={this.state.selectedVideo} />
+        <VideoList
+          onVideoSelect={selectedVideo => this.setState({selectedVideo})}
+          videos={this.state.videos}
+        />
       </div>
     );
   }
