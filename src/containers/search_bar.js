38d8bb590b5c17144cd00e184f5394665e694fdf
Jason Holmberg <jason.holmberg@experticity.com>
2017-01-08 21:54:00 -0700

    axios start

--- src/containers/search_bar.js
+++ src/containers/search_bar.js
@@ -17,9 +17,14 @@ export default class SearchBar extends Component {
     this.setState({term: event.target.value});
   }
 
+  onFormSubmit(event) {
+    event.preventDefault();
+    // we need to fetch the weather here
+  }
+
   render() {
     return (
-      <form className="input-group">
+      <form onSubmit={this.onFormSubmit} className="input-group">
         <input
           placeholder="Get a five-day forecast in your favorite cities"
           className="form-control"
